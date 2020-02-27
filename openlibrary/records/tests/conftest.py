b0ac5b0f27fd68c6b51d8ab0a4dc15a6bd5a1a1e
cclauss <cclauss@me.com>
2020-02-27 12:41:20 +0100

    Fix flake8 issues

--- openlibrary/records/tests/conftest.py
+++ openlibrary/records/tests/conftest.py
@@ -1,34 +1,37 @@
-def compare_results(request):
-        """Compare two objects d1 an d2 recursively skipping the 'key', "last_modified"
+def compare_results(d1, d2):
+    """Compare two objects d1 an d2 recursively skipping the 'key', "last_modified"
         and "revision" keys if present"""
-        for i in ["revision", "last_modified", "key"]:
-            if i in d1: d1.pop(i)
-            if i in d2: d2.pop(i)
+    for i in ["revision", "last_modified", "key"]:
+        if i in d1:
+            d1.pop(i)
+        if i in d2:
+            d2.pop(i)
 
-        # print compare_results.depth * 2 * "--" +  "Comparing\n","1 ==> ",d1, "\n2 ==> ", d2
-        if d1 == d2: # Trivially the same
-            return True
-        if isinstance(d1, list) and isinstance(d2, list) and len(d1) == len(d2):
-            for i,j in zip(d1, d2):
-                # compare_results.depth += 1
-                ret = compare_results(i, j)
-                # compare_results.depth -= 1
-                # print " ==> ",ret, "\n"
-                if ret:
-                    pass
-                else:
-                    return False
-            return True
+    # print(compare_results.depth * 2 * "--" +  "Comparing\n","1 ==> ",d1, "\n2 ==> ",
+    #       d2)
+    if d1 == d2:  # Trivially the same
+        return True
+    if isinstance(d1, list) and isinstance(d2, list) and len(d1) == len(d2):
+        for i, j in zip(d1, d2):
+            # compare_results.depth += 1
+            ret = compare_results(i, j)
+            # compare_results.depth -= 1
+            # print " ==> ",ret, "\n"
+            if ret:
+                pass
+            else:
+                return False
+        return True
 
-        if isinstance(d1, dict) and isinstance(d2, dict) and len(d1) == len(d2):
-            for k,v in d1.iteritems():
-                # compare_results.depth += 1
-                ret = compare_results(d1.get(k), d2.get(k))
-                # print " ==> ",ret, "\n"
-                # compare_results.depth -= 1
-                if ret:
-                    pass
-                else:
-                    return False
-            return True
-        return False
+    if isinstance(d1, dict) and isinstance(d2, dict) and len(d1) == len(d2):
+        for k, v in d1.iteritems():
+            # compare_results.depth += 1
+            ret = compare_results(d1.get(k), d2.get(k))
+            # print " ==> ",ret, "\n"
+            # compare_results.depth -= 1
+            if ret:
+                pass
+            else:
+                return False
+        return True
+    return False
