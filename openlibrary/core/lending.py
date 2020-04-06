9635449d479dd08b710fa23b100235c077f4c130
cclauss <cclauss@me.com>
2020-04-07 00:44:47 +0200

    assert False, e, url

--- openlibrary/core/lending.py
+++ openlibrary/core/lending.py
@@ -237,12 +237,14 @@ def get_availability(key, ids):
     :param list of str ids:
     :rtype: dict
     """
+    assert config_ia_availability_api_v2_url, "{}, {}, {}".format(
+        config_ia_availability_api_v2_url, key, ids
+    )
     url = '%s?%s=%s' % (config_ia_availability_api_v2_url, key, ','.join(ids))
     try:
         content = urllib.request.urlopen(url=url, timeout=config_http_request_timeout).read()
         return simplejson.loads(content.decode('utf-8')).get('responses', {})
-    except ValueError as e:
-        assert False, "{}, {}".format(e, url) # , type(content), content)
+    except Exception as e:
         return {'error': 'request_timeout', 'details': str(e)}
 
 def get_edition_availability(ol_edition_id):
