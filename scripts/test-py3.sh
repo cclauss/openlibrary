5606d29199651bd035a33c6f282667aca2f397dc
cclauss <cclauss@me.com>
2020-04-21 15:28:20 +0200

    Log exceptions in catalog/get_ia.py

--- scripts/test-py3.sh
+++ scripts/test-py3.sh
@@ -11,7 +11,7 @@ RETURN_CODE=$?
        
 pytest openlibrary/catalog/marc/tests/test_get_subjects.py || true
 pytest openlibrary/catalog/marc/tests/test_parse.py || true
-pytest openlibrary/tests/catalog/test_get_ia.py || true
+pytest --show-capture=all openlibrary/tests/catalog/test_get_ia.py || true
 pytest --show-capture=all openlibrary/coverstore/tests/test_doctests.py || true
 pytest --show-capture=all openlibrary/plugins/openlibrary/tests/test_home.py || true
 flake8 --exit-zero --count --select=E722 --show-source
